df70e85773fa0c965aa7acd2d602b3a2ff7a60d2
Silvio Cullice <grandepuffo09@gmail.com>
2026-04-28 20:21:11 +0200

    Add Firebase configuration and initialization

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getAuth }        from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+import { getFirestore }   from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyDxYwhMSlmsouvFnpRM4tfeiB6-Wexs2oU",
+  authDomain:        "setgo-app-39ed9.firebaseapp.com",
+  projectId:         "setgo-app-39ed9",
+  storageBucket:     "setgo-app-39ed9.firebasestorage.app",
+  messagingSenderId: "1027910520694",
+  appId:             "1:1027910520694:web:dacf48e3727cfbdae63457"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
